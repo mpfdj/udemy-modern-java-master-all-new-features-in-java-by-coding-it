a8a98ae1e4fb5284506a855438d750809c9b1c8f
TO11RC <miel.de.jaeger@ing.com>
2024-06-03 13:17:27 +0200

    Typo

--- modern-java-start/modern-java/explore-latest-java-features/src/test/java/com/modernjava/sealed/VehicleTest.java
+++ modern-java-start/modern-java/explore-latest-java-features/src/test/java/com/modernjava/sealed/VehicleTest.java
@@ -3,7 +3,6 @@ package com.modernjava.sealed;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
